ddb0aa3de901deabb4913d71189f316a2de8fecf
msararin <129656501+msararin@users.noreply.github.com>
2026-08-18 13:51:16 +0700

    Wire Experiment 3 semantic progress colors

--- app/case-studies/nbo-nrt-azure-databricks/layout.tsx
+++ app/case-studies/nbo-nrt-azure-databricks/layout.tsx
@@ -8,6 +8,7 @@ import { Experiment2StatusInjector } from "./experiment2-status-injector"
 import { Experiment2BStructureInjector } from "./experiment2b-structure-injector"
 import { Experiment3InvestigationTrailInjector } from "./experiment3-investigation-trail-injector"
 import { Experiment3DefaultViewInjector } from "./experiment3-default-view-injector"
+import { Experiment3SemanticProgressInjector } from "./experiment3-semantic-progress-injector"
 
 export default function NboNrtAzureDatabricksLayout({ children }: { children: ReactNode }) {
   return (
@@ -21,6 +22,7 @@ export default function NboNrtAzureDatabricksLayout({ children }: { children: Re
       <Experiment2BStructureInjector />
       <Experiment3InvestigationTrailInjector />
       <Experiment3DefaultViewInjector />
+      <Experiment3SemanticProgressInjector />
       {children}
     </>
   )
